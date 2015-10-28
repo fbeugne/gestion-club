9645378bc2af5f11fab09f0fd8f5d34dc9ed6bed
Fabien BEUGNE <fbeugne@gmail.com>
2015-10-28 07:54:14 +0100

    Mise à jour table du bureau

--- licencies/lister_bureau.php
+++ licencies/lister_bureau.php
@@ -5,6 +5,9 @@ include (WPINC . '/gestion-club/common.php');
 $gestion_saison = new GestionSaison();
 $saison_selectionnee = $gestion_saison->GetSaisonSelectionnee();
 
+echo "Composition du bureau pour la saison $saison_selectionnee";
+echo "<br>";
+echo "<br>";
 
 $ListeRole=array(
 "Président",
@@ -12,14 +15,7 @@ $ListeRole=array(
 "Secrétaire",
 "Vice-Secrétaire",
 "Trésorier",
-"Vice-Trésorier",
-"Bureau");
-
-
-$DonneesAAfficher = array(
-  "NOM" => array("NOM"),
-  "Prenom" => array("Prenom"),
-);
+"Vice-Trésorier");
 
 
 $conn_db = new BaseDeDonnesPalet();
@@ -31,22 +27,56 @@ foreach ($ListeRole as $role)
   
   $result = $conn_db->RequeteSQL($sql); 
   
-  if ($result)
+  // Affichage des rôles uniques
+  echo "<tr>";
+  if ( ($result) && ($info_bureau = $result->fetch_row()) )
   {
+    $nom=$info_bureau[0];
+    $prenom=$info_bureau[1];
     
-    while($info_bureau = $result->fetch_row())
-    {
-      $nom=$info_bureau[0];
-      $prenom=$info_bureau[1];
-      
+    echo "<td><b>$role</td>";
+    echo "<td>$nom</td>";
+    echo "<td>$prenom</td>";
+    echo "</tr>"; 
+  }
+  else
+  {
       echo "<tr>";
       echo "<td><b>$role</td>";
-      echo "<td>$nom</td>";
-      echo "<td>$prenom</td>";
+      echo "<td><i>Inconnu</td>";
+      echo "<td><i>Inconnu</td>";
       echo "</tr>"; 
-    }
   }
 }
+
+// Listing des membres du bureau
+$sql = "select NOM, Prenom from Licencies where (Licencies.`$saison_selectionnee` = 'Bureau') ORDER BY `Licencies`.`NOM` ASC";
+
+$result = $conn_db->RequeteSQL($sql); 
+
+if ($result)
+{
+  while($info_bureau = $result->fetch_row())
+  {
+    $nom=$info_bureau[0];
+    $prenom=$info_bureau[1];
+    
+    echo "<tr>";
+    echo "<td><b>Membre</td>";
+    echo "<td>$nom</td>";
+    echo "<td>$prenom</td>";
+    echo "</tr>"; 
+  }
+}
+else
+{
+    echo "<tr>";
+    echo "<td><b>Membre</td>";
+    echo "<td><i>Inconnu</td>";
+    echo "<td><i>Inconnu</td>";
+    echo "</tr>";
+}
+  
 echo "</table>";
 
 ?>
