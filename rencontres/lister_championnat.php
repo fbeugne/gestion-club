c9c162f1d593788de982c5ab0e603aa80ae151b2
Fabien BEUGNE <fbeugne@gmail.com>
2015-10-28 07:54:51 +0100

    Listing des résultats coupe et championnat en un seul tableau

--- rencontres/lister_championnat.php
+++ rencontres/lister_championnat.php
@@ -13,26 +13,18 @@ $annee2 = $gestion_saison->GetAnneeSelectionnee();
 $DonneesAAfficher = array(
   "Date" => array("Date"),
   "Adversaire" => array("Lieu"),
+  "Déplacement" => array(" <i>", "CONVERT(REPLACE(REPLACE(Dpct,-1,'En déplacement'),0,'A domicile'),CHAR(20))"),
   "Nb joueurs" => array("nb_joueurs"),
   "Nb adversaires" => array("nb_adversaires"),
   "Résultats" => array("points_pour", " / ", "points_contre")
 );
 
-echo "Résultats des matchs de championnat à domicile  pour la saison $annee1 / $annee2";
+echo "Résultats des matchs de championnat pour la saison $annee1 / $annee2";
 echo "<br>";
 echo "<br>";
 
 
-$sql_from = "Dates where ( (Dates.`Type` = 'Championnat' or Dates.`Type` = 'Amical') and Dates.`Dpct` = 0 and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
-
-$conn_db->AfficherTable($DonneesAAfficher, $sql_from);
-
-
-echo "Résultats des matchs de championnat en déplacement  pour la saison $annee1 / $annee2";
-echo "<br>";
-echo "<br>";
-
-$sql_from = "Dates where ( (Dates.`Type` = 'Championnat' or Dates.`Type` = 'Amical') and Dates.`Dpct` = -1 and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
+$sql_from = "Dates where ( (Dates.`Type` = 'Championnat' or Dates.`Type` = 'Amical') and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
 
 $conn_db->AfficherTable($DonneesAAfficher, $sql_from);
 
--- rencontres/lister_coupe.php
+++ rencontres/lister_coupe.php
@@ -15,26 +15,18 @@ $annee2 = $gestion_saison->GetAnneeSelectionnee();
 $DonneesAAfficher = array(
   "Date" => array("Date"),
   "Adversaire" => array("Lieu"),
+  "Déplacement" => array(" <i>", "CONVERT(REPLACE(REPLACE(Dpct,-1,'En déplacement'),0,'A domicile'),CHAR(20))"),
   "Nb équipes" => array("nb_joueurs"),
   "Nb adversaires" => array("nb_adversaires"),
   "Résultats" => array("points_pour", " / ", "points_contre")
 );
 
-echo "Résultats des matchs de coupe à domicile  pour la saison $annee1 / $annee2";
+echo "Résultats des matchs de coupe pour la saison $annee1 / $annee2";
 echo "<br>";
 echo "<br>";
 
 
-$sql_from = "Dates where ( (Dates.`Type` = 'Coupe') and Dates.`Dpct` = 0 and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
-
-$conn_db->AfficherTable($DonneesAAfficher, $sql_from);
-
-
-echo "Résultats des matchs de coupe en déplacement  pour la saison $annee1 / $annee2";
-echo "<br>";
-echo "<br>";
-
-$sql_from = "Dates where ( (Dates.`Type` = 'Coupe') and Dates.`Dpct` = -1 and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
+$sql_from = "Dates where ( (Dates.`Type` = 'Coupe') and Dates.`Date` > '$annee1-08-01' and Dates.`Date` < '$annee2-08-01') ORDER BY Dates.`Date` ASC";
 
 $conn_db->AfficherTable($DonneesAAfficher, $sql_from);
 
